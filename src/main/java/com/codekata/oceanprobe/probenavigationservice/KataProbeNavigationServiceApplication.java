476e643aa47fb3c864f4c63e71df3c3957ff18da
jabeevulla <jabeevulla.cse@gmail.com>
2025-03-14 05:46:01 +0000

    ✅ Initialize project with Spring Boot setup

---
+++ src/main/java/com/codekata/oceanprobe/probenavigationservice/KataProbeNavigationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codekata.oceanprobe.probenavigationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataProbeNavigationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataProbeNavigationServiceApplication.class, args);
+	}
+
+}
